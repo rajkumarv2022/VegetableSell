115b1789bfb0608db1e808ea061eb79c2e25cfc4
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-11-18 17:51:15 +0530

    modifing landing page

--- farmerapi/src/component/LandinPage.tsx
+++ farmerapi/src/component/LandinPage.tsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-cover bg-center" style={{backgroundImage: "url('https://rare-gallery.com/uploads/posts/4600032-countryside-harvest-agriculture-farm-nature-field.jpg')"}}>
-      <div className="bg-white bg-opacity-50 rounded p-10">
-        <h1 className="text-5xl font-bold mb-8 text-green-800">Welcome to Farmer App</h1>
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div className="flex flex-col items-center justify-center bg-white bg-opacity-50 rounded-md p-10 border-2 border-gray-200 shadow-md">
+        <h1 className="text-5xl font-bold mb-8">Welcome to Farmer App</h1>
         <p className="text-2xl mb-12 text-gray-700">Explore our priceless cost of food market</p>
         <div className="flex space-x-4">
           <Link to="/login">
