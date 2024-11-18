88176931381918b2452e434909f2a8775e415db0
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-11-18 19:07:03 +0530

    modifying login and signup page

--- farmerapi/src/component/LoginPage.tsx
+++ farmerapi/src/component/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
@@ -51,43 +51,62 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{backgroundImage: "url('https://rare-gallery.com/uploads/posts/4600032-countryside-harvest-agriculture-farm-nature-field.jpg')"}}>
-      <div className="bg-white bg-opacity-75 rounded-lg shadow-xl p-10">
+    <div className="flex items-center justify-center h-screen bg-cover bg-center">
+      <div className="flex flex-col items-center justify-center w-full h-full max-w-96 max-h-[600px] gap-10 border-2 border-gray-400 rounded-md shadow-md">
         <h1 className="text-3xl font-semibold mb-4 text-center">Login Page</h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <form onSubmit={handleSubmit} className="flex flex-col items-center max-w-80 w-full gap-12">
           {error && <div className="text-red-500 text-sm">{error}</div>}
-          <input
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 px-3 py-2"
-          />
-          <input
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 px-3 py-2"
-          />
-          <div className="flex items-center">
-            <input
-              type="checkbox"
-              checked={isSeller}
-              onChange={(e) => setIsSeller(e.target.checked)}
-              className="form-checkbox h-5 w-5 text-blue-500"
-            />
-            <label className="ml-2 text-gray-700">Seller Account</label>
+
+ 
+          <div className='flex flex-col w-full gap-8'>
+              <input
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full px-3 py-3 outline-none border-2 border-gray-700"
+              />
+              <input
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-full px-3 py-3 outline-none border-2 border-gray-700"
+              /> 
+
+              <div className='flex flex-row gap-4'>
+                <input type="checkbox" checked={isSeller} onChange={(e) => setIsSeller(e.target.checked)} className="form-checkbox h-5 w-5 text-blue-500 border-2 border-gray-700"/>
+                <label className="ml-2">Seller Account</label>
+              </div>
+
+              
           </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 rounded-md transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-blue-200"
-            disabled={loading}
-          >
-            {loading ? 'Loading...' : 'Login'}
-          </button>
+          
+          <div className="flex flex-col w-full h-full items-between gap-8">
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white py-3 rounded-md"
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Login'}
+            </button>
+
+            <button
+              className="w-full bg-gray-500 hover:bg-gray-600 text-white py-3 rounded-md"
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Google'}
+            </button>
+
+            <div className='flex flex-row gap-2'>
+              <p className='ml-3'>If you don't have an account click</p>
+              <Link to='/Signup' className='text-blue-400'>SignUp</Link>
+            </div>
+          </div>
+
         </form>
       </div>
     </div>
--- farmerapi/src/component/SignupPage.tsx
+++ farmerapi/src/component/SignupPage.tsx
@@ -35,51 +35,68 @@ export default function SignupPage() {
   };
   
   return (
-    <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{backgroundImage: "url('https://rare-gallery.com/uploads/posts/4600032-countryside-harvest-agriculture-farm-nature-field.jpg')"}}>
-    <div className="bg-white bg-opacity-75 rounded-lg shadow-xl p-10">
-        <h1 className="text-2xl font-semibold mb-4">Signup</h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
+    <div className="flex items-center justify-center h-screen bg-cover bg-center">
+    <div className="flex flex-col w-full h-full max-w-96 max-h-[600px] items-center justify-center gap-4 border-2 border-gray-400 shadow-md rounded-md ">
+        <h1 className="text-3xl font-semibold mb-4">Signup</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center w-full gap-8">
           {error && <div className="text-red-500">{error}</div>}
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-          />
-          <label className="flex items-center">
+
+          <div className='flex flex-col w-full max-w-80 gap-8'>
             <input
-              type="checkbox"
-              checked={isSeller}
-              onChange={(e) => setIsSeller(e.target.checked)}
-              className="form-checkbox h-5 w-5 text-blue-500"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full px-3 py-3 border-2 border-gray-700 outline-none"
             />
-            <span className="ml-2 text-gray-700">Create Seller Account</span>
-          </label>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-          >
-            Signup
-          </button>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-3 py-3 border-2 border-gray-700 outline-none"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-3 border-2 border-gray-700 outline-none"
+            />
+
+            <div>
+              <label className="flex items-center">
+              <input
+                type="checkbox"
+                checked={isSeller}
+                onChange={(e) => setIsSeller(e.target.checked)}
+                className="form-checkbox h-5 w-5 border-2 border-gray-700"
+              />
+              <span className="ml-2 text-gray-700">Create Seller Account</span>
+            </label>
+            </div>
+          </div>
+
+          <div className='flex flex-col w-full max-w-80 gap-4'>
+            <button
+              type="submit"
+              className="w-full bg-blue-400 text-white py-3 rounded-md hover:bg-blue-600"
+            >
+              Signup
+            </button>
+
+            <button
+              type="submit"
+              className="w-full bg-gray-500 text-white py-3 rounded-md hover:bg-gray-600"
+            >
+              Signup with Google
+            </button>
+
+            <div className="felx flex-row w-full">  
+              <p className='flex flex-row gap-2 w-full max-w-72 ml-6'> <span>Already have an account click</span> <Link to="/login" className='text-blue-400'>Login</Link> </p>
+            </div>
+          </div>         
         </form>
-        <div className="mt-4">
-          <Link to="/login" className="text-blue-500 hover:underline">Already have an account? Login</Link>
-        </div>
       </div>
     </div>
   );
